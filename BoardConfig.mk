bb4823dba6e3e829bc3daa3f17525d468547dace
neXusPRIME <yasir.elec@gmail.com>
2013-01-12 23:57:22 +0500

    TWRP Config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@ USE_CAMERA_STUB := true
 -include vendor/htc/protou/BoardConfigVendor.mk
 
 TARGET_NO_BOOTLOADER := true
-TARGET_CPU_ABI := armeabi
+#TARGET_CPU_ABI := armeabi
 TARGET_BOOTLOADER_BOARD_NAME := protou
 TARGET_BOARD_PLATFORM := msm7k
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
@@ -31,9 +31,25 @@ BOARD_UMS_LUNFILE := /sys/class/android_usb/f_mass_storage/lun0/file
 # Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_INITRC := device/htc/protou/recovery/init.recovery.rc
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_10x18.h\"
-BOARD_CUSTOM_GRAPHICS := ../../../device/htc/protou/recovery/graphics.c
+#BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_10x18.h\"
+#BOARD_CUSTOM_GRAPHICS := ../../../device/htc/protou/recovery/graphics.c
 # Use this flag if the board has a ext4 partition larger than 2gb
 BOARD_HAS_LARGE_FILESYSTEM := true
 
+
+#TWRP
+# Flags
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60 -DQCOM_HARDWARE
+
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+DEVICE_RESOLUTION := 480x800
+TW_INCLUDE_DUMLOCK := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
 
