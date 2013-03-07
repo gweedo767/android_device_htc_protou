40abb4e8171a8910d896cf07b2d45bab5514597b
neXusPRIME <yasir.elec@gmail.com>
2013-03-08 01:17:43 +0500

    The Holy Grail

--- device_protou.mk
+++ device_protou.mk
@@ -17,34 +17,16 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
-#idc
-PRODUCT_COPY_FILES += \
-    device/htc/protou/prebuilt/usr/idc/himax-touchscreen.idc:system/usr/idc/himax-touchscreen.idc \
-    device/htc/protou/prebuilt/usr/idc/projector_input.idc:system/usr/idc/projector_input.idc \
-    device/htc/protou/prebuilt/usr/idc/qwerty.idc:system/usr/idc/qwerty.idc \
-    device/htc/protou/prebuilt/usr/idc/qwerty2.idc:system/usr/idc/qwerty2.idc \
-    device/htc/protou/prebuilt/usr/idc/projector_input.idc:system/usr/idc/projector_input.idc \
-    device/htc/protou/prebuilt/usr/keychars/Generic.kcm:system/usr/keychars/Generic.kcm \
-    device/htc/protou/prebuilt/usr/keychars/qwerty.kcm:system/usr/keychars/qwerty.kcm \
-    device/htc/protou/prebuilt/usr/keychars/qwerty2.kcm:system/usr/keychars/qwerty2.kcm \
-    device/htc/protou/prebuilt/usr/keychars/Virtual.kcm:system/usr/keychars/Virtual.kcm \
-    device/htc/protou/prebuilt/usr/keylayout/himax-touchscreen.kl:system/usr/keylayout/himax-touchscreen.kl \
-    device/htc/protou/prebuilt/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-    device/htc/protou/prebuilt/usr/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
-    device/htc/protou/prebuilt/usr/keylayout/protou-keypad.kl:system/usr/keylayout/protou-keypad.kl \
-    device/htc/protou/prebuilt/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
-    device/htc/protou/prebuilt/usr/keylayout/projector-Keypad.kl:system/usr/keylayout/projector-Keypad.kl
-
-PRODUCT_COPY_FILES += \
-    device/htc/protou/prebuilt/etc/vold.fstab:system/etc/vold.fstab
-
 # HTC blobs for recovery
+
 PRODUCT_COPY_FILES += \
+    device/htc/protou/recovery/init.protou.rc:recovery/root/init.protou.rc \
     device/htc/protou/recovery/sbin/choice_fn:recovery/root/sbin/choice_fn \
     device/htc/protou/recovery/sbin/detect_key:recovery/root/sbin/detect_key \
     device/htc/protou/recovery/sbin/offmode_charging:recovery/root/sbin/offmode_charging \
     device/htc/protou/recovery/sbin/power_test:recovery/root/sbin/power_test \
-    device/htc/protou/recovery/sbin/rmt_storage:recovery/root/sbin/rmt_storage
+    device/htc/protou/recovery/sbin/rmt_storage:recovery/root/sbin/rmt_storage \
+    device/htc/protou/recovery/sbin/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
 
 $(call inherit-product, build/target/product/full.mk)
 
