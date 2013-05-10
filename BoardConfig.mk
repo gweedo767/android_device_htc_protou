6ed2d817fb530666fa76c94d69bbb5629a00ca34
Nathan Grebowiec <njgreb@gmail.com>
2013-05-10 08:28:42 -0500

    add arch type

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,7 @@ TARGET_NO_BOOTLOADER := true
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH := arm
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_BOOTLOADER_BOARD_NAME := protou
@@ -41,13 +42,6 @@ TARGET_RECOVERY_INITRC := device/htc/protou/recovery/init.recovery.rc
 BOARD_CUSTOM_GRAPHICS := ../../../device/htc/protou/recovery/graphics.c
 # Use this flag if the board has a ext4 partition larger than 2gb
 BOARD_HAS_LARGE_FILESYSTEM := true
-
-
-#TWRP
-# Flags
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60 -DQCOM_HARDWARE
 
 
 DEVICE_RESOLUTION := 480x800
